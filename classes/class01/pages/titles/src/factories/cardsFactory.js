6170ae09e58a8727d0c1385da38e1509ad9c15c2
bduaart <contato.bduaart@gmail.com>
2023-03-01 00:54:05 -0300

    refactor and add class 01

--- classes/class01/pages/titles/src/factories/cardsFactory.js
+++ classes/class01/pages/titles/src/factories/cardsFactory.js
@@ -2,8 +2,7 @@ import CardsController from "./../controllers/cardsController.js"
 import CardsView from "./../views/cardsView.js"
 import CardsService from "./../services/cardsService.js"
 
-const cardListWorker = new Worker(`./src/workers/cardListWorker.js`, { type: "module" })
-
+const cardListWorker = new Worker(`./src/workers/cardListWorker.js`, {type: "module"})
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initialize() {
@@ -11,9 +10,7 @@ const factory = {
       view: new CardsView(),
       service: new CardsService({
         dbUrl: `${rootPath}/assets/database.json`,
-        cardListWorker
-
-      })
+        cardListWorker: cardListWorker })
     })
   }
 }
